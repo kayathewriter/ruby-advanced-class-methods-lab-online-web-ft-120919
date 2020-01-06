dbc68f7a6f2c0e45178f2a4baecd1d52b5d8e6d3
Mikael Kaya <kayaswork@gmail.com>
2020-01-06 04:17:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,11 @@ class Song
     self.class.all << self
   end
   
+  s = Song.new
+s.name = "The Ship Song"
+s.artist_name = "Nick Cave"
+s.save
+  
   song = Song.create
 Song.all.include?(song) 
 
